c47409ed2cb8d3c130b1936260d5e6d0b854fd6d
Matt Kokas <mattkokas@gmail.com>
2021-03-02 18:38:45 -0500

    adding discord widget

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,11 +2,15 @@
    <div>
       <Header />
 
-      <main class="main-content" role="main">
-         <slot />
+      <div class="content-wrapper">
+         <main class="main-content" role="main">
+            <slot />
 
-         <BackgroundNoise />
-      </main>
+            <BackgroundNoise />
+         </main>
+
+         <Sidebar />
+      </div>
 
       <Footer />
    </div>
@@ -27,6 +31,7 @@
    import Footer from './Footer.vue';
    import Header from './Header.vue';
    import Radio from './Radio.vue';
+   import Sidebar from './Sidebar.vue';
 
    export default {
       components: {
@@ -35,7 +40,8 @@
          Footer,
          Header,
          Radio,
-         Section
+         Section,
+         Sidebar
       },
       data () {
          return {
@@ -54,11 +60,33 @@
 </script>
 
 <style lang="scss">
-   .main-content {
-      position: relative;
+   .content-wrapper {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: stretch;
+      align-content: flex-start;
+      justify-content: flex-start;
+      width: 100%;
       background: linear-gradient(to right, rgba(36,31,31,1) 0%, rgba(36,31,31,1) 32%, rgba(74,71,70,1) 100%);
       padding: 2rem 0;
       box-shadow: inset 0 0 150px $black;
-      min-height: 60vh;
+      position: relative;
+      justify-content: center;
    }
+
+   .main-content {
+      position: relative;
+      min-height: 60vh;
+      width: 100%;
+
+      @include breakpoint($screen-l-min) {
+         width: 75%;
+      }
+
+      @include breakpoint($screen-l-min) {
+         width: 80%;
+      }
+   }
+
+
 </style>
---
+++ src/layouts/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+   <aside class="side-bar">
+      <h5 class="text-center">Radio Arcane on Discord</h5>
+
+      <iframe id="discord-widget" src="https://discord.com/widget?id=816408050268766218&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+   </aside>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+   .side-bar {
+      padding: 2rem 0;
+      margin-top: 2rem;
+      border-top: 1px solid hex-to-rgba($white-smoke, 0.4);
+      width: 100%;
+
+      @include breakpoint($screen-l-min) {
+         width: 25%;
+         padding-right: 30px;
+         border-top: 0;
+         margin-top: 0;
+         padding-top: 0;
+      }
+
+      @include breakpoint($screen-l-min) {
+         width: 20%;
+      }
+   }
+
+   #discord-widget {
+      margin: 0 auto;
+      max-width: 300px;
+   }
+</style>
